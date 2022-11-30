3189b0b2abf03429512d596b89e4d5f8c91d32f5
Hugaaaooo <hugo.petruzzelli25@gmail.com>
2022-11-30 10:24:11 +0100

    Systeme Zombie
    Boids + Deplacement

---
+++ Assets/Scipt/EnemyAI.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+
+
+
+public class EnemyAI : MonoBehaviour
+{
+	
+	NavMeshAgent nm; 
+	public Transform target; 
+
+    public float distanceTreshold = 10f;
+
+    public enum AIState { idle, chasing}; 
+
+    public AIState aiState = AIState.idle;
+           
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        nm = GetComponent <NavMeshAgent>();
+        StartCoroutine(Think());
+        target = GameObject.FindGameObjectWithTag("Player").transform; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    IEnumerator Think()
+
+    {
+        while (true)
+        {
+            switch (aiState)
+            {
+                case AIState.idle:
+                    float dist = Vector3.Distance(target.position, transform.position);
+                    if (dist < distanceTreshold)
+                    {
+                        aiState = AIState.chasing; 
+
+                    }
+                    nm.SetDestination(transform.position); 
+                    break;
+                case AIState.chasing:
+                     dist = Vector3.Distance(target.position, transform.position);
+                    if (dist > distanceTreshold )
+                    {
+                        aiState = AIState.idle;
+                    }
+
+                    nm.SetDestination(target.position);
+                    break;
+                default:
+                    break;
+            }
+
+            
+            yield return new WaitForSeconds(1f);
+
+        }
+    }
+
+
+}
